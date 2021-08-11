ed62a4fd87a4b2242e782a59202cc9a6d2885858
Muhammad Mukharir <mkharir18@gmail.com>
2021-08-11 14:40:30 +0700

    restructure with hapi plugin

---
+++ src/server.js
@@ -0,0 +1,42 @@
+const Hapi = require('@hapi/hapi');
+// const routes = require('./routes');
+
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+
+const notesPlugin = require('./notesPlugin');
+
+const init = async () => {
+  const notesService = new NotesService();
+ 
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+ 
+  // server.route(routes);
+
+  // registrasi satu plugin
+  // await server.register({
+  //   plugin: notesPlugin,
+  //   options: { notes: [] },
+  // });
+ 
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
+ 
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+
+};
+
+init();
